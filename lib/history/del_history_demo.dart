f6e4db147894abd6dd38d388f98effbfa85f713a
red-nose-jocker <12126913+red-nose-jocker@user.noreply.gitee.com>
2024-07-25 09:54:03 +0800

    优化

---
+++ lib/history/del_history_demo.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'del_history.dart';
+
+class DeleteHistoryDemo extends StatefulWidget {
+  const DeleteHistoryDemo({super.key});
+
+  @override
+  State<DeleteHistoryDemo> createState() => _DeleteHistoryDemoState();
+}
+
+class _DeleteHistoryDemoState extends State<DeleteHistoryDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: ListTile(
+        title: const Text("删除所有历史记录", style: TextStyle(fontSize: 30)),
+        trailing: IconButton(
+            icon: const Icon(Icons.delete_outline_sharp),
+            onPressed: () async {
+                DelHistory.deleteHistory();
+              }
+        ),
+      ),
+    );
+  }
+}
